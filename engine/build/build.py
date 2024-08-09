ed2366441fa586b34bc4835ea480e7a7842025e7
businessmajor <barrios.st@northeastern.edu>
2024-08-09 15:13:34 -0400

    fixed build script on windows a lil

--- engine/build/build.py
+++ engine/build/build.py
@@ -5,27 +5,23 @@ import subprocess
 # (1)==================== COMMON CONFIGURATION OPTIONS ======================= #
 COMPILER = "g++"
 SOURCE = "./src/*.cpp"
-EXECUTABLE = "main"
+EXECUTABLE = "engine"
 
 # (2)=================== Platform specific configuration ===================== #
 # For each platform we need to set the following items
 # Arguments needed for our program (Add others as you see fit)
-ARGUMENTS = "-std=c++20 "  # Specify compiler arguments
-INCLUDE_DIR = "-I ./include/ -I ./../common/thirdparty/glm "          # Which directories do we want to include.
+ARGUMENTS = "-std=c++20"  # Specify compiler arguments
+INCLUDE_DIR = "-I ./include/ -I ./../common/thirdparty/glm"          # Which directories do we want to include.
 LIBRARIES = "-lSDL3"            # What libraries do we want to include
 
 if platform.system() == "Linux":
-    ARGUMENTS += "-D LINUX"  # -D is a #define sent to preprocessor
-    # INCLUDE_DIR = ""
-    # LIBRARIES = "-lSDL3"
+    ARGUMENTS += " -D LINUX"  # -D is a #define sent to preprocessor
 elif platform.system() == "Darwin":
-    ARGUMENTS += "-D MAC"
-    # INCLUDE_DIR = "-I ./include/ -I ./../common/thirdparty/glm"
-    # LIBRARIES = "-lSDL3"
+    ARGUMENTS += " -D MAC"
 elif platform.system() == "Windows":
-    ARGUMENTS += "-D MINGW -static-libgcc -static-libstdc++"
+    ARGUMENTS += "-D MINGW -static-libgcc -static-libstdc++ -mwindows"
     sdl_cflags_libs = subprocess.getoutput("pkg-config sdl3 --cflags --libs")
-    INCLUDE_DIR += f"{sdl_cflags_libs}"
+    INCLUDE_DIR += f" {sdl_cflags_libs}"
     LIBRARIES = f"-lmingw32 -L ./lib"
     EXECUTABLE += ".exe"
 
--- learnopengl/build.py
+++ learnopengl/build.py
@@ -10,19 +10,18 @@ EXECUTABLE = "main"
 # (2)=================== Platform specific configuration ===================== #
 # For each platform we need to set the following items
 # Arguments needed for our program (Add others as you see fit)
-ARGUMENTS = "-std=c++20"  # Specify compiler arguments
-# Which directories do we want to include.
-INCLUDE_DIR = "-I ./include/"
-LIBRARIES = "-lSDL3"            # What libraries do we want to include
+ARGUMENTS = "-std=c++20"                                       # Specify compiler arguments
+INCLUDE_DIR = "-I ./include/ -I ./../common/thirdparty/glm"     # Which directories do we want to include.
+LIBRARIES = "-lSDL3"                                            # What libraries do we want to include
 
 if platform.system() == "Linux":
     ARGUMENTS += " -D LINUX"  # -D is a #define sent to preprocessor
 elif platform.system() == "Darwin":
     ARGUMENTS += " -D MAC"
 elif platform.system() == "Windows":
-    ARGUMENTS += " -D MINGW -static-libgcc -static-libstdc++"
+    ARGUMENTS += " -D MINGW -static-libgcc -static-libstdc++ -mwindows"
     sdl_cflags_libs = subprocess.getoutput("pkg-config sdl3 --cflags --libs")
-    INCLUDE_DIR += f"{sdl_cflags_libs}"
+    INCLUDE_DIR += f" {sdl_cflags_libs}"
     LIBRARIES = f"-lmingw32 -L ./lib"
     EXECUTABLE += ".exe"
 
